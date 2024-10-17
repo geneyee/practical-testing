66f5c32f2415338b24e65c981c02375041b3a487
geneyee <geneyeey@gmail.com>
2024-10-17 15:46:21 +0900

    테스트 케이스 세분화하기

--- src/test/java/sample/cafekiosk/unit/CafekioskTest.java
+++ src/test/java/sample/cafekiosk/unit/CafekioskTest.java
@@ -5,6 +5,7 @@ import sample.cafekiosk.unit.beverage.Americano;
 import sample.cafekiosk.unit.beverage.Latte;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CafekioskTest {
 
@@ -31,6 +32,30 @@ class CafekioskTest {
 		assertThat(cafeKiosk.getBeverages().size()).isEqualTo(1);
 		assertThat(cafeKiosk.getBeverages()).hasSize(1); // 위에랑 같은 뜻인데 .hasSize()는 list의 사이즈가 무엇인지
 		assertThat(cafeKiosk.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+	}
+
+	// 해피 케이스
+	@Test
+	void addSeveralBeverages() {
+		CafeKiosk cafeKiosk = new CafeKiosk();
+		Americano americano = new Americano();
+
+		cafeKiosk.add(americano, 2);
+
+		assertThat(cafeKiosk.getBeverages().get(0)).isEqualTo(americano);
+		assertThat(cafeKiosk.getBeverages().get(1)).isEqualTo(americano);
+	}
+
+	// 경계값 테스트
+	@Test
+	void addZeroBeverages() {
+		CafeKiosk cafeKiosk = new CafeKiosk();
+		Americano americano = new Americano();
+
+//		cafeKiosk.add(americano, 0);
+		assertThatThrownBy(() -> cafeKiosk.add(americano, 0))
+				.isInstanceOf(IllegalArgumentException.class)
+				.hasMessage("음료는 1잔 이상 주문하실 수 있습니다.");
 	}
 
 	@Test
