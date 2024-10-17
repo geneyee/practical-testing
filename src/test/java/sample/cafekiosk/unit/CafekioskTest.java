3fbde599416177d6015c2490f24c94c55b4c1aac
geneyee <geneyeey@gmail.com>
2024-10-17 16:48:44 +0900

    BDD

--- src/test/java/sample/cafekiosk/unit/CafekioskTest.java
+++ src/test/java/sample/cafekiosk/unit/CafekioskTest.java
@@ -1,5 +1,6 @@
 package sample.cafekiosk.unit;
 
+import org.hibernate.query.sqm.mutation.internal.cte.CteInsertStrategy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import sample.cafekiosk.unit.beverage.Americano;
@@ -94,8 +95,10 @@ class CafekioskTest {
 	}
 
 	// calculateTotalPrice() TDD로 구현하기
+	@DisplayName("주문 목록에 담긴 상품들의 총 금액을 계산할 수 있다.")
 	@Test
 	void calculateTotalPrice() {
+		// given
 		CafeKiosk cafeKiosk = new CafeKiosk();
 		Americano americano = new Americano();
 		Latte latte = new Latte();
@@ -103,7 +106,10 @@ class CafekioskTest {
 		cafeKiosk.add(americano);
 		cafeKiosk.add(latte);
 
+		// when
 		int totalPrice = cafeKiosk.calculateTotalPrice();
+
+		// then
 		assertThat(totalPrice).isEqualTo(8500);
 	}
 
